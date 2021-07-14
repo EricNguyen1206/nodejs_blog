b121e07d02027d44b7483c87e34477d866535e4f
Eric Nguyen <11a1eric3@gmail.com>
2021-07-14 14:54:48 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+const route = require('./routes/index');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+//HTTP logger
+// app.use(morgan('combined'));
+
+//Template engine
+app.engine('handlebars', handlebars());
+// exphbs.create({ extname=".hbs" });
+
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+//Route init
+route(app);
+
+app.listen(port, () =>
+    console.log(`Example app listening at http:/localhost:${port}`),
+);
